b634175517770380c7ce5b9b96e899b3c7b3f98e
krishnacr7united <krisha.22bcb7232@vitapstudent.ac.in>
2026-06-20 16:25:01 +0530

    Fix Kaggle download in train script

--- train_model.py
+++ train_model.py
@@ -1,10 +1,32 @@
+import os
+import warnings
+warnings.filterwarnings('ignore')
 import pandas as pd
 import numpy as np
 import pickle
-import warnings
-warnings.filterwarnings('ignore')
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+
+# Download dataset from Kaggle if not present
+if not os.path.exists('creditcard.csv'):
+    print("Downloading dataset from Kaggle...")
+    os.system('pip install kaggle --quiet')
+    
+    kaggle_dir = os.path.expanduser('~/.kaggle')
+    os.makedirs(kaggle_dir, exist_ok=True)
+    
+    username = os.environ.get('KAGGLE_USERNAME', '')
+    key = os.environ.get('KAGGLE_KEY', '')
+    
+    with open(os.path.join(kaggle_dir, 'kaggle.json'), 'w') as f:
+        f.write(f'{{"username":"{username}","key":"{key}"}}')
+    
+    os.system(f'chmod 600 {kaggle_dir}/kaggle.json')
+    os.system('kaggle datasets download -d mlg-ulb/creditcardfraud --unzip -p .')
+    print("Dataset downloaded!")
+
+if not os.path.exists('creditcard.csv'):
+    raise FileNotFoundError("creditcard.csv not found! Check Kaggle credentials.")
 
 df = pd.read_csv('creditcard.csv')
 
